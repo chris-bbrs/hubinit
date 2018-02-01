f77a2530cb33edf0e8ff1309fa544f680493e6e8
cnobody <chris.bbrs@gmail.com>
2018-02-01 13:26:06 +0200

    s flag for ssh option with set-url

--- crepo.sh
+++ crepo.sh
@@ -5,10 +5,11 @@ name=${PWD##*/}
 private='false'
 description=''
 
-while getopts 'pd:' flag; do
+while getopts 'pd:s' flag; do
   case "${flag}" in
     p) private='true' ;;
 	d) description="${OPTARG}";;
+	s) SSH='true' ;;
     *) error "Unexpected option ${flag}" ;;
   esac
 done
@@ -17,5 +18,10 @@ read -r -p "Enter host username:" username
 
 curl -u "$username" https://api.github.com/user/repos -d '{"name":"'$name'", "private":"'$private'", "description":"'$description'"}'
 
-git remote add origin https://github.com/$username/$name
+git remote add origin https://github.com/$username/$name.git
+
+if [ SSH ]; then
+	git remote set-url origin git@github.com:$username/$name.git
+fi
+
 git push origin master
