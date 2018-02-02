700e6e5c197c8731d78dd75a6e4a947bf819fdc3
cnobody <chris.bbrs@gmail.com>
2018-02-02 03:35:41 +0200

    add r flag for remote_name

--- crepo.sh
+++ crepo.sh
@@ -2,32 +2,34 @@
 
 set -e
 
-name=${PWD##*/}
+repo=${PWD##*/}
 private='false'
 description=''
 SSH='false'
+remote_name='origin'
 
-while getopts 'pd:sn:' flag; do
+while getopts 'pd:sn:r:' flag; do
   case "${flag}" in
-    p) private='true' ;;
+    p) private='true';;
 	d) description="${OPTARG}";;
-	s) SSH='true' ;;
-	n) name="${OPTARG}";;
-    *) error "Unexpected option ${flag}" ;;
+	s) SSH='true'git;;
+	n) repo="${OPTARG}";;
+	r) remote_name="${OPTARG}";;
+    *) error "Unexpected option ${flag}";;
   esac
 done
 
 read -r -p "Enter host username: " username
 read -rs -p "Enter host password for user '$username': " password
 
-curl -u "$username:$password" https://api.github.com/user/repos -d '{"name":"'$name'", "private":"'$private'", "description":"'$description'"}'
+curl -u "$username:$password" https://api.github.com/user/repos -d '{"name":"'$repo'", "private":"'$private'", "description":"'$description'"}'
 
-git remote add origin https://github.com/$username/$name.git
+git remote add $remote_name https://github.com/$username/$repo.git
 
 if $SSH ; then
-	git remote set-url origin git@github.com:$username/$name.git
-	git push origin master
+	git remote set-url $remote_name git@github.com:$username/$repo.git
+	git push $remote_name master
 else
-	expect -f expected.expect $username $password
+	expect -f expected.expect $username $password $remote_name
 fi
 
